5ce1884f4f5944b5d1b931f0cb4698565b9f5517
Mouad <mouadouchelh05@gmail.com>
2025-12-27 11:16:33 +0100

    adding equal btn fonctionnality

--- main.js
+++ main.js
@@ -1,29 +1,53 @@
+
 const input=document.getElementById('input');
 const numbers=document.getElementsByClassName('numbers');
+const operations = document.getElementsByClassName('operations');
+const clear = document.getElementById('clear');
+const deleteBtn = document.getElementById('delete');
+const parenl = document.getElementById('parenl');
+const parenr = document.getElementById('parenr');
+const equal = document.getElementById('equals');
+
+
+function Clear(){
+    input.value="";
+}
+
+
 for (let i = 0; i < numbers.length; i++){
     numbers[i].addEventListener('click',()=>{
+        input.value=="Error" || input.value=="Infinity"? Clear() : null;
         input.value+=numbers[i].value;
     });
 }
-const operations = document.getElementsByClassName('operations');
 for(let i = 0 ; i < operations.length;i++){
     operations[i].addEventListener('click',()=>{
+        input.value=="Error" || input.value=="Infinity"? Clear() : null;
         input.value+=operations[i].value;
     })
 }
-const clear = document.getElementById('clear');
-clear.addEventListener('click',()=>{
-    input.value="";
-});
-const deleteBtn = document.getElementById('delete');
+clear.addEventListener('click',Clear);
+
 deleteBtn.addEventListener('click',()=>{
+    input.value=="Error" || input.value=="Infinity"? Clear() : null;
     input.value=input.value.slice(0,input.value.length-1);
 })
-const parenl = document.getElementById('parenl');
-const parenr = document.getElementById('parenr');
+
 parenl.addEventListener('click',()=>{
+    input.value=="Error" || input.value=="Infinity"? Clear() : null;
     input.value+="(";
 });
 parenr.addEventListener('click',()=>{
+    input.value=="Error" || input.value=="Infinity"? Clear() : null;
     input.value+=")";
 });
+
+
+equal.addEventListener('click',()=>{
+    try{
+        input.value=eval(input.value);
+    }
+    catch{
+        input.value="Error";
+    }
+})
